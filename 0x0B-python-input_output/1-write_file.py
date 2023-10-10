7f4647358518af246ebad720a6da094f3550c89c
MihdarAbbasher <mihdar.ph@gmail.com>
2023-10-10 21:52:37 +0200

    t1 up

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,4 +6,4 @@
 def write_file(filename="", text=""):
     """write txt to file filename"""
     with open(filename, mode='w', encoding='utf-8') as myFile:
-        myFile.write(text)
+        return(myFile.write(text))
