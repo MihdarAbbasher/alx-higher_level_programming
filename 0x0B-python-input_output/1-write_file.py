b97e252d9bfb06b187f1012bff67e01a6e4ef3ed
MihdarAbbasher <mihdar.ph@gmail.com>
2023-10-10 21:57:27 +0200

    t2

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,13 @@
 
 
 def write_file(filename="", text=""):
-    """write txt to file filename"""
+    """write txt to file filename
+    Args:
+        filename (str): The name of the file.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters successfully written.
+
+    """
     with open(filename, mode='w', encoding='utf-8') as myFile:
         return(myFile.write(text))
