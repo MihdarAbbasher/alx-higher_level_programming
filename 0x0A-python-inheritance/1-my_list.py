7cc8bedfa49e73a2667282991ce77334448a068e
MihdarAbbasher <mihdar.ph@gmail.com>
2023-10-29 07:26:49 -0700

    t1 update init

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,6 +3,9 @@
 
 class MyList(list):
     """list class inherits from list"""
+    def __init___(self):
+        """init list"""
+        super().__init__()
     def print_sorted(self):
         """returns list of available attributes and methods of obj"""
         print(sorted(self))
