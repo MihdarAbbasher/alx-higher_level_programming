79601f9c4aaba0e7842b8c791d8b1f2542a1c05a
MihdarAbbasher <mihdar.ph@gmail.com>
2023-10-30 05:56:51 +0200

    t1 test

--- 0x0A-python-inheritance/tests/1-my_list.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Module defines lookup method."""
-
-class MyList(list):
-    """list class inherits from list"""
-    def print_sorted(self):
-        """returns list of available attributes and methods of obj"""
-        print(sorted(self))
