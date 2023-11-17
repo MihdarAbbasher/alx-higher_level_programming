a3a9266a420971903ed1a1e718aee4013ee8597c
MihdarAbbasher <mihdar.ph@gmail.com>
2023-11-17 23:04:53 +0200

    t11 - t16

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,4 @@
---  a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
+--  a script that creates the database hbtn_0d_usa and the table cities
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
   `id` INT UNIQUE NOT NULL AUTO_INCREMENT,
