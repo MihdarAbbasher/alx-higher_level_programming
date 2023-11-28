715d91a31031e0aba7964101924ad152579d9682
MihdarAbbasher <mihdar.ph@gmail.com>
2023-11-28 01:04:27 -0800

    t100 update

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -5,5 +5,5 @@ SELECT tv_shows.title, tv_genres.name
   ON tv_shows.id = tv_show_genres.show_id
   LEFT JOIN tv_genres
   ON tv_show_genres.genre_id = tv_genres.id
-  WHERE tv_shows.title != "Dexter"
+  WHERE tv_shows.title <> 'Dexter'
   ORDER BY tv_shows.title, tv_genres.name;
