22e2b5ac870141ebf14264e1fa275513a500e1ab
MihdarAbbasher <mihdar.ph@gmail.com>
2023-12-05 03:34:51 -0800

    tes

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
-""" Module for test Base class """
+""" Module for test Rectangle class """
 import unittest
-from models.base import Base
+from models.rectangle import Rectangle
 
 
-class TestBaseMethods(unittest.TestCase):
-    """ class to test Base class """
+class TestRectangleMethods(unittest.TestCase):
+    """ class to test Rectangle class """
 
     def setUp(self):
         """ Method calles for all tests """
-        Base._Base__nb_objects = 0
+        Rectangle._Base__nb_objects = 0
 
     def test_id(self):
         """ Test assigned id """
-        new = Base(1)
-        self.assertEqual(new.id, 1)
+        new = Rectangle(1, 2)
+        self.assertEqual(new.width, 1)
+        self.assertEqual(new.height, 2)
 
     def test_id_default(self):
         """ Test default id """
-        new = Base()
-        self.assertEqual(new.id, 1)
+        new = Rectangle(1, 2, 3)
+        self.assertEqual(new.x, 3)
 
     def test_id_nb_objects(self):
         """ Test nb object attribute """
-        new = Base()
-        new2 = Base()
-        new3 = Base()
-        self.assertEqual(new.id, 1)
-        self.assertEqual(new2.id, 2)
-        self.assertEqual(new3.id, 3)
+        new = Rectangle(1, 2, 3, 4)
+        self.assertEqual(new.y, 4)
 
+    def test_id(self):
+        """test id of inst."""
+        new = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(new.id, 5)
