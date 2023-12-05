086b6465e90d3dc400fc0f5293d98432c4381853
MihdarAbbasher <mihdar.ph@gmail.com>
2023-12-05 03:00:10 -0800

    tests

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Module for test Base class """
+import unittest
+from models.base import Base
+
+
+class TestBaseMethods(unittest.TestCase):
+    """ class to test Base class """
+
+    def setUp(self):
+        """ Method calles for all tests """
+        Base._Base__nb_objects = 0
+
+    def test_id(self):
+        """ Test assigned id """
+        new = Base(1)
+        self.assertEqual(new.id, 1)
+
+    def test_id_default(self):
+        """ Test default id """
+        new = Base()
+        self.assertEqual(new.id, 1)
+
+    def test_id_nb_objects(self):
+        """ Test nb object attribute """
+        new = Base()
+        new2 = Base()
+        new3 = Base()
+        self.assertEqual(new.id, 1)
+        self.assertEqual(new2.id, 2)
+        self.assertEqual(new3.id, 3)
+
