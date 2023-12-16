09d5ee9b3a6325bf5f7a7bcda1ee6cf544edd18b
MihdarAbbasher <mihdar.ph@gmail.com>
2023-12-16 12:32:08 +0200

    t3

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa
+ Usage: ./0-select_states.py <mysql username>
+  <mysql password> <database name >
+  ascending order by state.id
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    quer = db.cursor()
+    quer.execute("SELECT * FROM `states` WHERE name = {} ORDER BY states.id".format(sys.argv[4]))
+    [print(state) for state in quer.fetchall()]
