e2a2ba17b0f538208f7e879dac1b05dffce34cda
MihdarAbbahser <mihdar.ph@gmail.com>
2023-12-17 04:06:54 -0800

    t5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa
+ Usage: ./0-select_states.py <mysql username>
+  <mysql password> <database name >
+  ascending order by state.id
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    query = "SELECT * FROM `cities`\
+            inner join states\
+            on cities.state_id = states.id\
+            WHERE states.name = {} ORDER BY cities.id".format(sys.argv[4])
+    cur.execute(query)
+    [print(x) for x in cur.fetchall()]
