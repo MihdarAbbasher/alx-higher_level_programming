8d78a18c1da9739c4ab26d57a685003a8818786c
MihdarAbbasher <mihdar.ph@gmail.com>
2023-12-30 12:23:06 +0200

    t7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,8 @@
+exports.nbOccurences = function (list, searchElement) {
+  let l = list.length;
+  let res = 0;
+  for (let i = 0; i < l; i++) {
+    if (list[i] === searchElement) { res++; }
+  }
+  return (res);
+}
