ebc6d4008ddc1f45a6120b1fefc8fd8a463246ca
MihdarAbbasher <mihdar.ph@gmail.com>
2023-12-31 07:18:05 +0200

    style

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -10,16 +10,19 @@ module.exports = class Rectangle {
     this.width = w;
     this.height = h;
   }
+
   print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
+
   rotate () {
     let tmp = this.height;
     this.height = this.width;
     this.width = tmp;
   }
+
   double () {
     this.height = this.height * 2;
     this.width = this.width * 2;
