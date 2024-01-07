f464ef8f32e5bdd2b7183276db46fe70ad06c711
MihdarAbbasher <mihdar.ph@gmail.com>
2024-01-07 03:33:36 -0800

    t1up

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""  takes in a URL, sends a request to the URL and displays the value of the X-Request-Id"""
+"""  takes in a URL, sends a request to the URL and displays the value of
+the X-Request-Id"""
 
 if __name__ == "__main__":
     import urllib.request
@@ -7,4 +8,4 @@ if __name__ == "__main__":
 
     url = sys.argv[1]
     with urllib.request.urlopen(url) as res:
-        print (dict(res.headers).get('X-Request-Id'))
+        print(dict(res.headers).get('X-Request-Id'))
