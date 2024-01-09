5f05211327a3439eb6542a33dabcab5a900a1693
MihdarAbbasher <mihdar.ph@gmail.com>
2024-01-09 06:25:54 -0800

    102

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const file = require('fs');
+const file1 = file.readFileSync(process.argv[2], 'utf8');
+const file2 = file.readFileSync(process.argv[3], 'utf8');
+file.writeFileSync(process.argv[4], file1 + file2);
