5cedf338102233e2f49c8f37f76a45b673fd017a
MihdarAbbasher <mihdar.ph@gmail.com>
2024-01-10 04:20:24 -0800

    t100

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Finds a peak in a list of unsorted integers
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    Args:
+        list_of_integers(int): list of integers to find peak of
+    Returns: the peak of list_of_integers or None
+    """
+    pass
